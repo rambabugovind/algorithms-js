c7f0d0bd68a7fdabc8570294a1f8a84d39a7cecf
rambabugovind <rambabugovind@hotmail.com>
2018-05-23 17:50:35 -0500

    JS-Algos

--- smallest_positive_no.js
+++ smallest_positive_no.js
@@ -8,7 +8,7 @@ function solution(A) {
     let n = 1;
     for(let i=0; i<s.length;i++) {
     		if(s[0]<1) {
-        	s.splice(0,1);
+        	s.splice(0,1);//splice(index_pos,number_of_items_to_remove)
         }
      }
      for(let i=0;i<s.length;i++) {
